720e68d83ab400bb02a53f642762cdbd18926c1c
Washington Kimani <101510408+Washington-Kimani@users.noreply.github.com>
2023-09-29 15:20:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const methodOverride = require('method-override');
+require('dotenv');
+dotenv.config();
 
 const port = process.env.PORT || 5000
 
